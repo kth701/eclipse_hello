eda32e449f2227e1fbab5b34dd02deda2ce3c909
kth701 <kth701@nate.com>
2018-12-19 13:54:15 +0900

    GitHub for git pull

--- Hello_World/src/PushPrint.java
+++ Hello_World/src/PushPrint.java
@@ -6,6 +6,7 @@ public class PushPrint {
 		System.out.println("Add Java Class File Push");
 		System.out.println("World with Miku: 로컬 저장소에서 추가");
 		System.out.println("World with 42: 원격저장소에서 추가");
+		System.out.println("World with Hatsune : 로컬에서 병합결과 pull이후 원격저장소에서 추가");
 	}
 
 }
